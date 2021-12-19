79bafc1ea290cb8d4f0567eb017dec547461f6e8
saul milu <saulmsvarao@gmail.com>
2021-12-18 21:35:35 -0300

    sub rotas

---
+++ src/components/Alocacoes.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <div>
+        <p> Alocacoes</p>
+        <router-link :to="{name : 'alocForm'}">
+            <p>
+                para form
+            </p>
+        </router-link>
+        <router-link :to="{name : 'alocList'}">
+            <p>
+                para list
+            </p>
+        </router-link>
+        <router-view>
+
+        </router-view>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    name : 'Alocacoes'
+
+}
+</script>
+
+<style>
+
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 import Geral from './components/Geral'
 import Home from './components/Home'
+import AlocList from "./components/alocacoes/list";
+import AlocForm from "./components/alocacoes/form";
+import Alocacoes from "./components/Alocacoes";
 
 export const routes = [
     {path : '/geral', component : Geral},
-    {path : '/home', component : Home}
+    {path : '/home', component : Home},
+    {path : '/alocacoes', component : Alocacoes, children : [{path : '', name: 'alocList' ,component : AlocList}, {path: 'form', name: 'alocForm', component : AlocForm}]}
 ]
 
