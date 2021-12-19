d377067032257776655f84466260bf80ba8cd751
saul milu <saulmsvarao@gmail.com>
2021-12-18 21:34:56 -0300

    sublist in menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,17 +2,17 @@
     <div>
 
         <v-app-bar app>
-                <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
-                <v-toolbar-title>{{title}}</v-toolbar-title>
-            </v-app-bar>
+            <v-toolbar-title>{{title}}</v-toolbar-title>
+        </v-app-bar>
         <v-navigation-drawer
             v-model="drawer"
             absolute
             temporary
             >
-            <v-list  nav  dense >
-                <v-list-item-group v-model="group"  active-class="deep-purple--text text--accent-4" >
+            <v-list  >
+                <!-- <v-list-item-group v-model="group"  active-class="deep-purple--text text--accent-4" >
                     <v-list-item v-for="item in menus" v-bind:key="item.title">
                         <router-link :to="item.link">
                             <v-list-item-icon>
@@ -21,7 +21,23 @@
                             <v-list-item-title>{{item.title}}</v-list-item-title>
                         </router-link>
                     </v-list-item>
-                </v-list-item-group>
+                </v-list-item-group> -->
+               
+                    <v-list-group :value="true" v-for="menu in menus" v-bind:key="menu.title" no-action >
+                        <template v-slot:activator>
+                            <v-list-item-title>{{menu.title}}</v-list-item-title>
+                        </template>
+                        <v-list-item v-for="item in menu.subgroup" v-bind:key="item.title" :to="item.link" active-class="deep-purple--text text--accent-4" >
+                            <!-- <router-link :to="item.link"> -->
+                                <v-list-item-icon>
+                                    <v-icon>mdi-{{item.mdi}}</v-icon>
+                                </v-list-item-icon>
+                                <v-list-item-title>{{item.title}}</v-list-item-title>
+                            <!-- </router-link> -->
+                        </v-list-item>
+                    </v-list-group>
+                
+
                 
             </v-list>
         </v-navigation-drawer>
@@ -58,7 +74,7 @@ export default {
 
     };
   },
-  created() {},
+  created() { console.log(this.itensMenu);},
   props: ["itensMenu", "titlemenu"],
 };
 </script>
