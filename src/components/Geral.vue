c4d9be30fc36230f1750e8ac2a0c86a14300df2e
saul milu <saulmsvarao@gmail.com>
2021-12-18 21:36:18 -0300

    container row col and card

--- src/components/Geral.vue
+++ src/components/Geral.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <!-- <div>
 
         <p>
             {{ posEspaco }}
@@ -9,7 +9,22 @@
            <p>{{item.description}}</p>
            <br>
        </div>
-    </div>
+    </div> -->
+    <v-container>
+        <v-row v-for="(item, index) in items" :key="index">
+            <v-col v-for="sub in item" :key="sub.name">
+                <v-card  class="mx-auto"   max-width="400">
+                    <v-img  class="white--text align-end" height="200px" :src="sub.src">
+                        <v-card-title>{{sub.name}}</v-card-title>
+                    </v-img>
+                    <v-card-text>
+                        <div>{{sub.description}}</div>
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+
 </template>
 
 <script>
@@ -26,13 +41,28 @@ export default {
     },
     created(){
         getCharacters().then(res => {
-            var novosItems = []
-            for ( var it of res){
-                it.src = it.thumbnail.path + '/portrait_small.'+ it.thumbnail.extension;
-                novosItems.push(it)
-
+            // var novosItems = []
+            // var col = []
+            var rows = []
+            // rows.push(col)
+            for(var a = 0; a < res.length; a+=2){
+                res[a].src = res[a].thumbnail.path + '/landscape_medium.'+ res[a].thumbnail.extension;
+                res[a+1].src = res[a+1].thumbnail.path + '/landscape_medium.'+ res[a+1].thumbnail.extension;
+                rows.push( [ res[a], res[a+1] ] )
             }
-            this.items = novosItems
+
+            // for ( var it of res){
+                // if(col.length == 2){
+                //     rows.push(col)
+                //     col = []
+                // }
+                // rows.push(it)
+                // novosItems.push(it)
+            // }
+            // if(col.length == 1){
+            //     rows.push(col)
+            // }
+            this.items = rows
         });
     },
     
